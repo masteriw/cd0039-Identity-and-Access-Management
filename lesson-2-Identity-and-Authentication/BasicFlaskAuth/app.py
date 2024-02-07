ba14d4ae58f5c87275bb9ed741eb78c14f8a5b3b
masteriw <masteriw@gmail.com>
2024-02-07 20:48:41 -0300

    Finalmente funciona

--- lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
+++ lesson-2-Identity-and-Authentication/BasicFlaskAuth/app.py
@@ -4,12 +4,22 @@ import json
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import logging
+
+# Configurando o logging para o nível DEBUG
+logging.basicConfig(level=logging.DEBUG)
 
 # Inicializando a aplicação Flask
 app = Flask(__name__)
 
-'''As constantes AUTH0_DOMAIN, ALGORITHMS e API_AUDIENCE são definidas 
-com os valores apropriados para a sua aplicação Auth0.'''
+app.logger.handlers = []
+
+print('logging: ' + str(logging.getLogger().getEffectiveLevel()))
+
+'''
+As constantes AUTH0_DOMAIN, ALGORITHMS e API_AUDIENCE são definidas 
+com os valores apropriados para a sua aplicação Auth0.
+'''
 AUTH0_DOMAIN = 'masteriw.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'TestApi'
@@ -32,6 +42,7 @@ Em seguida, ela retorna o token de acesso.
 def get_token_auth_header():
     auth = request.headers.get('Authorization', None)
     if not auth:
+        logging.debug('Authorization header missing')
         raise AuthError({
             'code': 'authorization_header_missing',
             'description': 'Authorization header is expected.'
@@ -39,24 +50,28 @@ def get_token_auth_header():
 
     parts = auth.split()
     if parts[0].lower() != 'bearer':
+        logging.debug('Invalid header: Authorization header must start with "Bearer"')
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization header must start with "Bearer".'
         }, 401)
 
     elif len(parts) == 1:
+        logging.debug('Invalid header: Token not found')
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Token not found.'
         }, 401)
 
     elif len(parts) > 2:
+        logging.debug('Invalid header: Authorization header must be bearer token')
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization header must be bearer token.'
         }, 401)
 
     token = parts[1]
+    logging.debug('Token obtained from Authorization header')
     return token
 
 '''
@@ -69,9 +84,12 @@ reivindicações (claims) são corretas.
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
+    print('jwks' + str(jwks))
     unverified_header = jwt.get_unverified_header(token)
+    print('CHEGUEI')
     rsa_key = {}
     if 'kid' not in unverified_header:
+        logging.debug('Kid not in unverified header')
         raise AuthError({
             'code': 'invalid_header',
             'description': 'Authorization malformed.'
@@ -99,27 +117,32 @@ def verify_decode_jwt(token):
             return payload
 
         except jwt.ExpiredSignatureError:
+            logging.debug('Token expired')
             raise AuthError({
                 'code': 'token_expired',
                 'description': 'Token expired.'
             }, 401)
 
         except jwt.JWTClaimsError:
+            logging.debug('Invalid claims')
             raise AuthError({
                 'code': 'invalid_claims',
                 'description': 'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
-        except Exception:
+        except Exception as e:
+            logging.debug('Unable to parse authentication token: %s', e)
             raise AuthError({
                 'code': 'invalid_header',
                 'description': 'Unable to parse authentication token.'
             }, 400)
+    logging.debug('Unable to find the appropriate key')
     raise AuthError({
                 'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
             }, 400)
 
-'''Esta é um decorador que é usado para garantir que uma rota específica 
+'''
+Esta é um decorador que é usado para garantir que uma rota específica 
 requer autenticação. Ela obtém o token de acesso, verifica e decodifica 
 o JWT, e então chama a função original com o payload do JWT como um 
 argumento adicional.
@@ -129,17 +152,20 @@ def requires_auth(f):
     def wrapper(*args, **kwargs):
         token = get_token_auth_header()
         try:
+            print('token antes de verify_decode_jwt: ' + token)
             payload = verify_decode_jwt(token)
-        except:
+        except Exception as e:
+            print('Exception: ' + str(e))
             abort(401)
         return f(payload, *args, **kwargs)
 
     return wrapper
 
-'''Esta é uma rota que requer autenticação. Ela usa o decorador requires_auth, 
-imprime o payload do JWT e retorna “Access Granted”.'''
+'''
+Esta é uma rota que requer autenticação. Ela usa o decorador requires_auth, 
+imprime o payload do JWT e retorna “Access Granted”.
+'''
 @app.route('/headers')
 @requires_auth
 def headers(payload):
-    print(payload)
     return 'Access Granted'
